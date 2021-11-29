8b3d7341a1ee0d5ed1077cf5ea0c8f910d81883b
ElperdonXV <davidemartinografico@gmail.com>
2021-11-29 17:32:12 +0100

    all done

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -1,6 +1,13 @@
 //Il software deve chiedere per 10 volte all’utente di inserire un numero.
 //Il programma stampa la somma di tutti i numeri inseriti.
 
+
+//for(i=0; i<10; i++){
+    //let num = parseInt(prompt('Inserisci un numero'));
+    //let somma = parseInt(somma + num);
+//}
+//console.log(somma);
+
 let i=0;
 let somma = parseInt(0);
 while (i<10){
